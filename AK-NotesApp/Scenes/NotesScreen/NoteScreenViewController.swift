d76c50608e480a9e76f98c4dba2d7935b09b4828
Anıl <anilkaramis@gmail.com>
2024-11-05 23:10:32 +0300

    Create NoteScreenViewController and NoteScreenViewModel

--- AK-NotesApp/Scenes/NotesScreen/NoteScreenViewController.swift
+++ AK-NotesApp/Scenes/NotesScreen/NoteScreenViewController.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-class NoteScreenViewController:UIViewController {
+class NotesScreenViewController: UIViewController {
     
     private let headerLabel = UILabel()
     private let addNoteButton = UIButton(type: .system)
@@ -19,7 +19,9 @@ class NoteScreenViewController:UIViewController {
         return collectionView
     }()
     
-    init(viewModel: NotesScreenViewModel) {
+    private let viewModel: NotesScreenViewModel
+    
+    init(with viewModel: NotesScreenViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
     }
@@ -27,5 +29,114 @@ class NoteScreenViewController:UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    private let viewModel: NotesScreenViewModel
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setup()
+        bindToViewModel()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setupBackBarButtonItem()
+    }
+    
+    @objc
+    private func handleAddNoteButton() {
+        viewModel.goToCreateNote()
+    }
+    
+    private func setup() {
+        setupSuperView()
+        setupHeaderLabel()
+        setupNotesCollection()
+        setupAddNoteButton()
+    }
+    
+    private func setupSuperView() {
+        view.backgroundColor = .backgroundApp
+    }
+    
+    private func setupHeaderLabel() {
+        view.addSubview(headerLabel)
+        
+        headerLabel.text = viewModel.textForHeaderLabel
+        headerLabel.textColor = .headerText
+        headerLabel.textAlignment = .left
+        headerLabel.font = UIFont.systemFont(ofSize: 30, weight: .medium)
+        
+        headerLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(60)
+            make.leading.equalToSuperview().offset(60)
+        }
+    }
+    
+    private func setupNotesCollection() {
+        view.addSubview(notesCollection)
+        
+        notesCollection.backgroundColor = .clear
+        notesCollection.clipsToBounds = true
+        notesCollection.showsVerticalScrollIndicator = false
+        notesCollection.dataSource = self
+        notesCollection.delegate = self
+        notesCollection.register(NoteViewCell.self, forCellWithReuseIdentifier: NoteViewCell.identifier)
+        
+        notesCollection.snp.makeConstraints { make in
+            make.top.equalTo(headerLabel.snp.bottom).offset(20)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+        
+    }
+    
+    private func setupAddNoteButton() {
+        view.addSubview(addNoteButton)
+        
+        addNoteButton.setImage(UIImage(systemName: "square.and.pencil"), for: .normal)
+        addNoteButton.imageView?.layer.transform = CATransform3DMakeScale(1, 1, 1)
+        addNoteButton.tintColor = .white
+        addNoteButton.backgroundColor = .appColor
+        addNoteButton.layer.cornerRadius = 65 / 2
+        addNoteButton.addTarget(self, action: #selector(handleAddNoteButton), for: .touchUpInside)
+        
+        addNoteButton.snp.makeConstraints { make in
+            make.height.width.equalTo(65)
+            make.trailing.bottom.equalToSuperview().inset(40)
+        }
+    }
+    
+    private func setupBackBarButtonItem() {
+        let backBarButtonItem = UIBarButtonItem(title: "Notes", style: .plain, target: self, action: nil)
+        backBarButtonItem.tintColor = .appColor
+        self.navigationItem.backBarButtonItem = backBarButtonItem
+    }
+}
+
+private extension NotesScreenViewController {
+    private func bindToViewModel() {
+        viewModel.didGoToNextScreen = { [weak self] viewController in
+            self?.navigationController?.pushViewController(viewController, animated: true)
+        }
+    }
+}
+
+extension NotesScreenViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        viewModel.cellViewModels.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NoteViewCell.identifier, for: indexPath) as? NoteViewCell else {
+            return NoteViewCell()
+        }
+        
+        cell.configure(with: viewModel.cellViewModels[indexPath.item])
+        return cell
+    }
+}
+
+extension NotesScreenViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        viewModel.goToEditNote(at: indexPath.item)
+    }
 }
--- AK-NotesApp/Scenes/NotesScreen/NotesScreenViewModel.swift
+++ AK-NotesApp/Scenes/NotesScreen/NotesScreenViewModel.swift
@@ -6,7 +6,52 @@
 //
 
 import Foundation
+import UIKit
 
 class NotesScreenViewModel {
-    
-}
+        
+        var didGoToNextScreen: ((UIViewController) -> Void)?
+        
+        private(set) var textForHeaderLabel = "Notes"
+        
+        let cellViewModels: [NoteViewCellViewModel]
+        let notes = [Note(title: "Title 1", text: """
+    First example not for app. So I want to improve about Notes.app but I must investigate about app. 
+    """, dateCreated: "Dec 12, 2022", dateModified: "Dec 12, 2022"),
+                     Note(title: "Title 2", text: "some text 1", dateCreated: "Feb 18, 2004", dateModified: "Dec 12, 2022"),
+                     Note(title: "Title 2", text: "some text 1", dateCreated: "Feb 18, 2004", dateModified: ""),
+                     Note(title: "Title 2", text: "some text 1", dateCreated: "Feb 18, 2004", dateModified: "Dec 12, 2022"),
+                     Note(title: "Title 2", text: "some text 1", dateCreated: "Feb 18, 2004", dateModified: ""),
+                     Note(title: "Title 2", text: "some text 1", dateCreated: "Feb 18, 2004", dateModified: ""),
+                     Note(title: "Title 2", text: "some text 1", dateCreated: "Feb 18, 2004", dateModified: "Dec 12, 2022"),
+                     Note(title: "Title 2", text: "some text 1", dateCreated: "Feb 18, 2004", dateModified: ""),
+                     Note(title: "Title 2", text: "some text 1", dateCreated: "Feb 18, 2004", dateModified: "Dec 12, 2022"),
+                     Note(title: "Title 2", text: "some text 1", dateCreated: "Feb 18, 2004", dateModified: ""),
+                     Note(title: "Title 2", text: "some text 1", dateCreated: "Feb 18, 2004", dateModified: ""),
+                     Note(title: "Title 2", text: "some text 1", dateCreated: "Feb 18, 2004", dateModified: "Dec 12, 2022"),
+                     Note(title: "Title 2", text: "some text 1", dateCreated: "Feb 18, 2004", dateModified: ""),
+                     Note(title: "Title 2", text: "some text 1", dateCreated: "Feb 18, 2004", dateModified: "Dec 12, 2022"),
+                     Note(title: "Title 2", text: "some text 1", dateCreated: "Feb 18, 2004", dateModified: ""),
+                     Note(title: "Title 2", text: "some text 1", dateCreated: "Feb 18, 2004", dateModified: "Dec 12, 2022"),
+                     Note(title: "Title 2", text: "some text 1", dateCreated: "Feb 18, 2004", dateModified: "Dec 12, 2022"),
+                     Note(title: "Title 2", text: "some text 1", dateCreated: "Feb 18, 2004", dateModified: "Dec 12, 2022"),
+                     Note(title: "Title 2", text: "some text 1", dateCreated: "Feb 18, 2004", dateModified: "Dec 12, 2022"),
+                     Note(title: "Title 2", text: "some text 1", dateCreated: "Feb 18, 2004", dateModified: "Dec 12, 2022"),
+                    ]
+        
+        init() {
+            cellViewModels = notes.map { NoteViewCellViewModel(titleNote: $0.title, textNote: $0.text, dateCreated: $0.dateCreated, dateModified: $0.dateModified) }
+        }
+        
+        func goToCreateNote() {
+            let viewModel = CreateNoteViewModel(note: nil)
+            let viewController = CreateNoteViewController(with: viewModel)
+            didGoToNextScreen?(viewController)
+        }
+        
+        func goToEditNote(at index: Int) {
+            let viewModel = CreateNoteViewModel(note: notes[index])
+            let viewController = CreateNoteViewController(with: viewModel)
+            didGoToNextScreen?(viewController)
+        }
+    }
