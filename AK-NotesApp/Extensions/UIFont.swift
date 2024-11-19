fb0df90e4159d08b4ab830edbfc826334814ab9d
Anıl <anilkaramis@gmail.com>
2024-11-19 16:24:04 +0300

    Update app fonts

---
+++ AK-NotesApp/Extensions/UIFont.swift
@@ -0,0 +1,18 @@
+//
+//  UIFont.swift
+//  AK-NotesApp
+//
+//  Created by AnılKaramış on 19.11.2024.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+    static var titleNote = UIFont(name: "KohinoorDevanagari-Semibold", size: 26)
+    static var previewTitleNote = UIFont(name: "KohinoorDevanagari-Regular", size: 21)
+    
+    static var textNote = UIFont.systemFont(ofSize: 19, weight: .regular)
+    static var previewTextNote = UIFont.systemFont(ofSize: 16)
+}
+
